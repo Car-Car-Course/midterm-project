ef5f9b3d26c5a05fa0250a8f95e9125f54fe7340
rayray2002 <rayray2002.huang@gmail.com>
2024-04-03 15:54:11 +0800

    fix extern macro

---
+++ arduino/midterm_project/node.h
@@ -0,0 +1,14 @@
+/***************************************************************************/
+// File			  [node.h]
+// Author		  [Erik Kuo, Joshua Lin]
+// Synopsis		[Code for managing car movement when encounter a node]
+// Functions  [/* add on your own! */]
+// Modify		  [2020/03/027 Erik Kuo]
+/***************************************************************************/
+
+/*===========================import variable===========================*/
+int extern _Tp;
+/*===========================import variable===========================*/
+
+// TODO: add some function to control your car when encounter a node
+// here are something you can try: left_turn, right_turn... etc.
